53857e088d6d32f1af243157de1f722f5cb76130
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-03 21:47:27 -0800

    Feat: Added search, navbar and background support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFontLoader from 'webfontloader';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+WebFontLoader.load({
+  google: {
+    families: ['Lato:100,300,400,500,700'],
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
