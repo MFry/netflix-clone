a340122508c3af00c5590123bdae9343a9deefac
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-03 16:49:50 -0800

    Feat: App renders only the navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import './main.css';
 
 const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+  <div>
+    <Navbar />
   </div>
 );
 
