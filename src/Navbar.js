3408f6f2208ec69d4547bbe9da15cb626e8c11d8
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-04 17:35:41 -0800

    Feat: Added navigation and styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation';
 import Search from './Search';
 import netflixLogo from './Netflix-logo.svg';
 
 const Navbar = props => (
   <header id="Header">
     <img src={netflixLogo} id="Logo" alt="Logo" />
+    <Navigation />
     <Search />
   </header>
 );
---
+++ src/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navigation = () => (
+  <nav id="Navigation">
+    <a href="#">Browse</a>
+    <a href="#">My List</a>
+  </nav>
+);
+export default Navigation;
