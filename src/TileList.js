eafb20c7fc15abdba39d17df172768768f5c3fb1
Michal Frystacky <Michal.Frystacky@alaska.gov>
2017-09-07 18:11:34 -0800

    Feat: Fixed fetch call and Carousel

--- src/TileList.js
+++ src/TileList.js
@@ -7,32 +7,34 @@ import Tile from './Tile';
 class TitleList extends Component {
   constructor(props) {
     super(props);
-    this.data = [];
-    // Move this
+    this.state = { data: [] };
+  }
+
+  componentWillMount() {
     axios
       .get(
         'https://api.themoviedb.org/3/discover/movie?api_key=87dfa1c669eea853da609d4968d294be&include_video=true&page=1&year=2017&sort_by=popularity.desc',
       )
       .then((response) => {
-        this.data = response.data.results;
+        const data = response.data.results;
+        this.setState({ data });
       })
       .catch((error) => {
-        this.data = {};
         console.log(error);
       });
   }
 
   render() {
-    let content = '';
-    if (this.data) {
-      content = this.data.map(movie => <Tile imgUrl={movie.backdrop_path} title={movie.title} />);
+    const data = this.state.data;
+    if (data.length > 0) {
+      const content = data.map(movie => <Tile imgUrl={movie.backdrop_path} title={movie.title} />);
+      return (
+        <Carousel className="TileList" axis="horizontal" howArrows dynamicHeight emulateTouch>
+          {content}
+        </Carousel>
+      );
     }
-
-    return (
-      <Carousel axis="horizontal" howArrows dynamicHeight emulateTouch>
-        {content}
-      </Carousel>
-    );
+    return <div />;
   }
 }
 export default TitleList;
