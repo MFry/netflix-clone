8510ee0a77823e826b13d4c02ffc7d2029e7fa6b
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-10 20:05:36 -0800

    Feat: Hero element codestub  with Billboard

--- src/Billboard.js
+++ src/Billboard.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import theCrownImage from './the-crown-background.jpg';
 
-const Billboard = () => <div id="Main" />;
+const Billboard = () => (
+  <div id="Main">
+    <div id="Billboard" />
+    <img id="Hero" src={theCrownImage} alt="TV show The Crown" />
+  </div>
+);
 export default Billboard;
