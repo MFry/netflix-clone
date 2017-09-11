fa9dbb636aa7a60503a97440aaa8b681bf0a4b87
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-10 23:23:42 -0800

    Feat: Flex display of movie images
    Bug- Cannot correctly resize flex boxes and it forces all of them on screen

--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
+// <p className="legend">{title}</p>
 const Tile = ({ imgUrl, title }) => (
   <div className="item">
     <img src={`https://image.tmdb.org/t/p/original${imgUrl}`} alt={`Movie ${title} backdrop`} />
-    <p className="legend">{title}</p>
   </div>
 );
 export default Tile;
