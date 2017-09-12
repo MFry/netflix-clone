db97f93b728a6c1a3df3d2f106ace144e01b6160
Michal Frystacky <Michal.Frystacky@alaska.gov>
2017-09-11 18:36:43 -0800

    Feat: Added support for custom classes in Tile

--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 // <p className="legend">{title}</p>
-const Tile = ({ imgUrl, title }) => (
-  <div className="item">
-    <img src={`https://image.tmdb.org/t/p/original${imgUrl}`} alt={`Movie ${title} backdrop`} />
-  </div>
-);
+const Tile = ({ imgUrl, title, className }) => {
+  const cssClasses = className ? `${className} item` : 'item';
+  return (
+    <div className={cssClasses}>
+      <img src={`https://image.tmdb.org/t/p/original${imgUrl}`} alt={`Movie ${title} backdrop`} />
+    </div>
+  );
+};
 export default Tile;
