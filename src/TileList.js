9ad15ce5a9d67bc2b6eff82a10fb610197718664
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-13 23:22:57 -0800

    Feat: Programmatically setup flex order

--- src/TileList.js
+++ src/TileList.js
@@ -8,7 +8,8 @@ import Carousel from './Carousel';
 class TitleList extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { movies: [] };
+    this.startCarousel = -1;
   }
 
   componentWillMount() {
@@ -18,23 +19,52 @@ class TitleList extends Component {
       )
       .then((response) => {
         const data = response.data.results;
-        this.setState({ data });
+        const movies = this.renderMovieTiles(data);
+        this.setState({ movies });
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
-  render() {
-    const data = this.state.data;
+  shiftRight() {
+    for (let i = 0; i < this.movies.length; ++i) {}
+  }
+
+  renderMovieTiles(data) {
+    let movies = [];
     if (data.length > 0) {
-      const content = data.map(movie => (
-        <Tile key={movie.id} imgUrl={movie.backdrop_path} title={movie.title} />
-      ));
-      content[content.length - 1] = React.cloneElement(content[content.length - 1], {
+      const order = new Array(data.length);
+      for (let i = 0; i < data.length; ++i) {
+        order[i] = (i + 2) % data.length;
+      }
+      let i = -1;
+      movies = data.map((movie) => {
+        i += 1;
+        return (
+          <Tile
+            key={movie.id}
+            imgUrl={movie.backdrop_path}
+            title={movie.title}
+            style={{ order: order[i] }}
+          />
+        );
+      });
+      /*
+      React.cloneElement(content[content.length - 1], {
         className: 'is-ref',
       });
-      return <Carousel>{content}</Carousel>;
+
+      movies[movies.length - 1].props.style = { order: '1' };
+      */
+      this.startCarousel = movies.length - 1;
+    }
+    return movies;
+  }
+
+  render() {
+    if (this.state.movies.length > 0) {
+      return <Carousel>{this.state.movies}</Carousel>;
     }
     return <div />;
   }
