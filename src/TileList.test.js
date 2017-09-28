e735e15763792fb1cfa9a03e9b4cf31a026ddb91
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-27 17:12:01 -0800

    Feat: Added snapshot testing to html rendering

--- src/TileList.test.js
+++ src/TileList.test.js
@@ -15,8 +15,12 @@ describe('TileList', () => {
     // axios.get = jest.fn(url => get(url));
     jest.mock('./api/__mocks__/axios');
     const waitForSample = createWaitForElement(Carousel);
-    expect.assertions(1);
+    expect.assertions(2);
     const wrapper = mount(<TileList />);
-    return waitForSample(wrapper).then(wrapper => expect(wrapper.state().movies.length).toBe(20));
+    return waitForSample(wrapper).then((wrapper) => {
+      const movies = wrapper.find(Carousel).html();
+      expect(wrapper.state().movies.length).toBe(20);
+      return expect(movies).toMatchSnapshot();
+    });
   });
 });
