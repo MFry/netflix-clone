6130dd80ed37406725feeae49d1e45f6316f402b
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-09-28 17:20:27 -0800

    Test: Added testing of individiual  props style

--- src/TileList.test.js
+++ src/TileList.test.js
@@ -3,6 +3,7 @@ import { shallow, mount } from 'enzyme';
 import { createWaitForElement } from 'enzyme-wait';
 
 import TileList from './TileList';
+import Tile from './Tile';
 import Carousel from './Carousel';
 
 describe('TileList', () => {
@@ -11,6 +12,7 @@ describe('TileList', () => {
     const wrapper = shallow(<TileList />);
     expect(wrapper.find('div').length).toBe(1);
   });
+
   it('Displays movies once the api resolves', () => {
     // axios.get = jest.fn(url => get(url));
     jest.mock('./api/__mocks__/axios');
@@ -21,6 +23,24 @@ describe('TileList', () => {
       const movies = wrapper.find(Carousel).html();
       expect(wrapper.state().movies.length).toBe(20);
       return expect(movies).toMatchSnapshot();
+    });
+  });
+  it('Movie Carousel shifts right by one after one call', () => {
+    jest.mock('./api/__mocks__/axios');
+    const waitForSample = createWaitForElement(Carousel);
+    expect.assertions(1);
+    const wrapper = mount(<TileList />);
+    return waitForSample(wrapper).then((wrapper) => {
+      const moviesBefore = wrapper.find(Tile);
+      wrapper.instance().shiftRight();
+      const moviesAfter = wrapper.find(Tile);
+      expect(moviesBefore.length).toEqual(moviesAfter.length);
+      for (let i = 0; i < moviesBefore.length - 1; ++i) {
+        const before = moviesBefore.nodes[i].props.style;
+        const after = moviesAfter.nodes[i].props.style;
+      }
+      return expect(wrapper.state().movies.length).toBe(20);
+      // return expect(moviesAfter).toMatchSnapshot();
     });
   });
 });
