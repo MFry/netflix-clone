af4945a49d8ebde01cbf2c95c705138291da055e
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-10-04 22:24:01 -0800

    Fix: Propagated style to Tile entities

--- src/Tile.js
+++ src/Tile.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 // <p className="legend">{title}</p>
-const Tile = ({ imgUrl, title, className }) => {
+const Tile = ({ imgUrl, title, className, style }) => {
   const cssClasses = className ? `${className} item` : 'item';
   return (
-    <div className={cssClasses}>
+    <div className={cssClasses} style={style}>
       <img src={`https://image.tmdb.org/t/p/original${imgUrl}`} alt={`Movie ${title} backdrop`} />
     </div>
   );
