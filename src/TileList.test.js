497d934b44dbb0b0ad3acdecc952e54068c0de38
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-10-08 23:46:57 -0800

    Test: Split shiftRight test into two seperate tests
    First, testing the initial carousel setup and all the contained values
    Second, testing the single application of shiftRight on the carousel

--- src/TileList.test.js
+++ src/TileList.test.js
@@ -25,19 +25,47 @@ describe('TileList', () => {
       return expect(movies).toMatchSnapshot();
     });
   });
-  it('Movie Carousel shifts right by one after one call', () => {
+  it('Sets up the initial carousel correctly', () => {
+    const correctFlexOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0];
     jest.mock('./api/__mocks__/axios');
     const waitForSample = createWaitForElement(Carousel);
-    expect.assertions(1);
+    expect.hasAssertions();
     const wrapper = mount(<TileList />);
     return waitForSample(wrapper).then((wrapper) => {
-      const moviesBefore = wrapper.find(Tile);
+      const initialCarousel = wrapper.find(Tile);
+      const initialCarouselStyles = initialCarousel.nodes.map(node => node.props.style);
+      expect(initialCarouselStyles.lenght).toEqual(correctFlexOrder.lenght);
+      expect(
+        initialCarouselStyles.reduce((accumulator, style) => Object.keys(style).length === 1, true),
+      ).toBeTruthy();
+      for (let i = 0; i < initialCarouselStyles.length; ++i) {
+        const curStyle = initialCarouselStyles[i].order;
+        const correctStyle = correctFlexOrder[i];
+        expect(curStyle).toEqual(correctStyle);
+      }
+      return expect(wrapper.state().movies.length).toBe(20);
+    });
+  });
+  it('First call of shiftRight Correctly updates the carousel', () => {
+    const correctOrder = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+    jest.mock('./api/__mocks__/axios');
+    const waitForSample = createWaitForElement(Carousel);
+    expect.hasAssertions();
+    const wrapper = mount(<TileList />);
+    return waitForSample(wrapper).then((wrapper) => {
       wrapper.instance().shiftRight();
-      const moviesAfter = wrapper.find(Tile);
-      expect(moviesBefore.length).toEqual(moviesAfter.length);
-      for (let i = 0; i < moviesBefore.length - 1; ++i) {
-        const before = moviesBefore.nodes[i].props.style;
-        const after = moviesAfter.nodes[i].props.style;
+      const moviesAfterRotation = wrapper.find(Tile);
+      expect(moviesAfterRotation.length).toEqual(correctOrder.length);
+      const afterRotationStyle = moviesAfterRotation.nodes.map(node => node.props.style);
+      // Ensure the style object structure hasn't changed
+      expect(
+        afterRotationStyle.reduce((accumulator, style) => Object.keys(style).length === 1, true),
+      ).toBeTruthy();
+      // Ensure the carousel rotated correctly
+      for (let i = 0; i < afterRotationStyle.length; ++i) {
+        const curStyle = afterRotationStyle[i].order;
+        const correctStyle = correctOrder[i];
+        expect(curStyle).toEqual(correctStyle);
       }
       return expect(wrapper.state().movies.length).toBe(20);
       // return expect(moviesAfter).toMatchSnapshot();
